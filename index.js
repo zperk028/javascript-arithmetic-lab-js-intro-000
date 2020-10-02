5fcf5971ae413a90ace81867c873017c3a3104bb
Zachary Perkins <ezmac028@gmail.com>
2020-10-02 18:42:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,11 +26,14 @@ function divide (a, b) {
   return a / b
 }
 
-
 function increment(n) {
   return n+= 1
 }
 
 function decrement(n) {
   return n-= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
 }
